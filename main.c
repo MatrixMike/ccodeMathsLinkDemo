600372395651228a20da11a0623e463cd2089adc
MatrixMike <matroxmike@icloud.com>
2016-10-28 08:24:31 +1100

    used indent -read comment about -lm compile directive

--- main.c
+++ main.c
@@ -12,23 +12,23 @@
 
 //  #define number 19
 
-int main() {
-int cv;
+int main()
+{
+	int cv;
 
 // for (cv=-3;cv=+3;cv++); {
-printf("length of array {i.e. numLEDS} is %d\n", numLEDS);
-for (cv=0;cv < numLEDS*2;cv++) {
+	printf("length of array {i.e. numLEDS} is %d\n", numLEDS);
+	for (cv = 0; cv < numLEDS * 2; cv++) {
 
-	printf("hi mike  %2d\n", cv);
-//	printf(" from include file: number = %d\n", number);
-//	printf("\n");
-	printf(" [%d]", ledarray[cv] );
-		printf(" %d=>", cv % numLEDS );
-	
-}
-	return 0 ;
-}
+		printf("hi mike  %2d\n", cv);
+//      printf(" from include file: number = %d\n", number);
+//      printf("\n");
+		printf(" [%d]", ledarray[cv]);
+		printf(" %d=>", cv % numLEDS);
 
+	}
+	return 0;
+}
 
 // int ledarray [] {2,3,9,10,11,13}; // 13 is onboard  {2,3,9,10,11,13}
 // const int numLEDS =  6; // 4 ;
--- screenDiagonal.c
+++ screenDiagonal.c
@@ -29,17 +29,16 @@
 
 int main(int argc, char **argv)
 {
-	double h,v,d,t;
-	
-	h=14.8;
-	v=12.0;
-//	d=19.0;
-	t=h*h;
-	t=20.0;
-	
+	double h, v, d, t;
+
+	h = 14.8;
+	v = 12.0;
+//      d=19.0;
+	t = h * h;
+	t = 20.0;
+
 	printf("Screen Diagonal %f\n", sqrt(19.0));
-//		printf("Screen Diagonal %f\n", sqrt(h*h + v*v));
-printf("Screen Diagonal %f\n", sqrt(t));
+//              printf("Screen Diagonal %f\n", sqrt(h*h + v*v));
+	printf("Screen Diagonal %f\n", sqrt(t));
 	return 0;
 }
-
